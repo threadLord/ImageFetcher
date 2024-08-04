1b987a5ab6d1a9af259dc7f5d5e638b4ba9c86b7
Marko Mutavdzic <belikompijuter@gmail.com>
2024-08-04 15:21:07 +0200

    Initial Commit

---
+++ ImageDownloader/ImageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloaderApp.swift
+//  ImageDownloader
+//
+//  Created by Marko on 4.8.24..
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
